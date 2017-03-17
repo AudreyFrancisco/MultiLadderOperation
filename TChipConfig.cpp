530dabd6a823c948ae07dfad801c9abdfdca653e
Andry Rakotozafindrabe <andry.malala.rakotozafindrabe@cern.ch>
2017-03-17 18:34:18 +0100

    Move global constants from namespace to static const data members of the class. Remove reference to TConfig. A TChipConfig no longer has access to other TChipConfig instances through TConfig. So the check of slave is deported to TSetup.

--- TChipConfig.cpp
+++ TChipConfig.cpp
@@ -3,118 +3,162 @@
 #include <stdio.h>
 #include <iostream>
 
-using namespace ChipConfig;
+using namespace std;
 
-TChipConfig::TChipConfig (TConfig *config, int chipId, const char *fName) {
-  fConfig           = config;
-  fChipId           = chipId;
-  fEnabled          = true;
-  fReceiver         = -1;
-  fControlInterface = -1;
- 
-  // fill default values from header file
-  fVCASN   = VCASN;
-  fVCASN2  = VCASN2;
-  fVCLIP   = VCLIP;
-  fVRESETD = VRESETD;
-  fITHR    = ITHR;
-  fIBIAS   = IBIAS;
+const int  TChipConfig::VCASN   = 50;
+const int  TChipConfig::VCASN2  = 64;
+const int  TChipConfig::VCLIP   = 0;
+const int  TChipConfig::VRESETD = 147;
+const int  TChipConfig::ITHR    = 51;
+const int  TChipConfig::IBIAS   = 64;
+const int  TChipConfig::VCASP   = 86;
 
-  fReadoutMode         = READOUT_MODE;
-  fEnableClustering    = ENABLE_CLUSTERING;
-  fMatrixReadoutSpeed  = MATRIX_READOUT_SPEED;
-  fSerialLinkSpeed     = SERIAL_LINK_SPEED;
-  fEnableSkewingGlobal = ENABLE_SKEWING_GLOBAL;
-  fEnableClockGating   = ENABLE_CLOCK_GATING;
-  fEnableCMUReadout    = ENABLE_CMU_READOUT;
-  
-  fStrobeDuration      = STROBE_DURATION;
-  fStrobeGap           = STROBE_GAP;
-  fStrobeDelay         = STROBE_DELAY;
-  fTriggerDelay        = TRIGGER_DELAY;
-  fPulseDuration       = PULSE_DURATION;
-  
-  fDclkReceiver        = DCLK_RECEIVER;
-  fDclkDriver          = DCLK_DRIVER;
-  fMclkReceiver        = MCLK_RECEIVER;
-  fDctrlReceiver       = DCTRL_RECEIVER;
-  fDctrlDriver         = DCTRL_DRIVER;
-  
-  fPreviousId          = PREVIOUS_ID;
-  fInitialToken        = INITIAL_TOKEN;
-  fDisableManchester   = DISABLE_MANCHESTER;
-  fEnableDdr           = ENABLE_DDR;
- 
-  if (fName) {
-    // read information from file
-  }
+const bool TChipConfig::READOUT_MODE           = false;// triggered
+const bool TChipConfig::ENABLE_CLUSTERING      = true;
+const int  TChipConfig::MATRIX_READOUT_SPEED   = 1;
+const int  TChipConfig::SERIAL_LINK_SPEED      = 1200;
+const bool TChipConfig::ENABLE_SKEWING_GLOBAL  = false;
+const bool TChipConfig::ENABLE_SKEWING_STARTRO = false;
+const bool TChipConfig::ENABLE_CLOCK_GATING    = false;
+const bool TChipConfig::ENABLE_CMU_READOUT     = false;
 
-  InitParamMap();
-}
+// timing values, to be refined
+const int  TChipConfig::STROBE_DURATION = 80;       // 2 us
+const int  TChipConfig::STROBE_GAP      = 4000;     // .1 ms
+const int  TChipConfig::STROBE_DELAY    = 20;       // 500 ns
+const int  TChipConfig::TRIGGER_DELAY   = 0;
+const int  TChipConfig::PULSE_DURATION  = 500;      // 12.5 us
 
+const int  TChipConfig::DCLK_RECEIVER   = 10;
+const int  TChipConfig::DCLK_DRIVER     = 10;
+const int  TChipConfig::MCLK_RECEIVER   = 10;
+const int  TChipConfig::DCTRL_RECEIVER  = 10;
+const int  TChipConfig::DCTRL_DRIVER    = 10;
 
-void TChipConfig::InitParamMap () 
+const int  TChipConfig::PREVIOUS_ID        = 0x10;
+const bool TChipConfig::INITIAL_TOKEN      = true;
+const bool TChipConfig::DISABLE_MANCHESTER = false;
+const bool TChipConfig::ENABLE_DDR         = true;
+
+//___________________________________________________________________
+TChipConfig::TChipConfig (int chipId, const char *fName)
 {
-  fSettings["CHIPID"]           = &fChipId;
-  fSettings["RECEIVER"]         = &fReceiver;
-  fSettings["CONTROLINTERFACE"] = &fControlInterface;
-  fSettings["ENABLED"]          = &fEnabled;
-  fSettings["ITHR"]             = &fITHR;
-  fSettings["IDB"]              = &fIDB;
-  fSettings["VCASN"]            = &fVCASN;
-  fSettings["VCASN2"]           = &fVCASN2;
-  fSettings["VCLIP"]            = &fVCLIP;
-  fSettings["VRESETD"]          = &fVRESETD;
-  fSettings["IBIAS"]            = &fIBIAS;
-  fSettings["VCASP"]            =  &fVCASP;
-  fSettings["VPULSEL"]          = &fVPULSEL;
-  fSettings["VPULSEH"]          = &fVPULSEH;
-  fSettings["VRESETP"]          = &fVRESETP;
-  fSettings["VTEMP"]            = &fVTEMP;
-  fSettings["IAUX2"]            = &fIAUX2;
-  fSettings["IRESET"]           = &fIRESET;
-  fSettings["STROBEDURATION"]   = &fStrobeDuration;
-  fSettings["PULSEDURATION"]    = &fPulseDuration;
-  fSettings["STROBEDELAYCHIP"]  = &fStrobeDelay;
-  fSettings["READOUTMODE"]      = (int*)&fReadoutMode;
-  fSettings["LINKSPEED"]        = &fSerialLinkSpeed; 
+    fChipId           = chipId;
+    fEnabled          = true;
+    fReceiver         = -1;
+    fControlInterface = -1;
+    
+    // fill default values from constants
+    fVCASN   = VCASN;
+    fVCASN2  = VCASN2;
+    fVCLIP   = VCLIP;
+    fVRESETD = VRESETD;
+    fITHR    = ITHR;
+    fIBIAS   = IBIAS;
+    
+    fReadoutMode         = READOUT_MODE;
+    fEnableClustering    = ENABLE_CLUSTERING;
+    fMatrixReadoutSpeed  = MATRIX_READOUT_SPEED;
+    fSerialLinkSpeed     = SERIAL_LINK_SPEED;
+    fEnableSkewingGlobal = ENABLE_SKEWING_GLOBAL;
+    fEnableClockGating   = ENABLE_CLOCK_GATING;
+    fEnableCMUReadout    = ENABLE_CMU_READOUT;
+    
+    fStrobeDuration      = STROBE_DURATION;
+    fStrobeGap           = STROBE_GAP;
+    fStrobeDelay         = STROBE_DELAY;
+    fTriggerDelay        = TRIGGER_DELAY;
+    fPulseDuration       = PULSE_DURATION;
+    
+    fDclkReceiver        = DCLK_RECEIVER;
+    fDclkDriver          = DCLK_DRIVER;
+    fMclkReceiver        = MCLK_RECEIVER;
+    fDctrlReceiver       = DCTRL_RECEIVER;
+    fDctrlDriver         = DCTRL_DRIVER;
+    
+    fPreviousId          = PREVIOUS_ID;
+    fInitialToken        = INITIAL_TOKEN;
+    fDisableManchester   = DISABLE_MANCHESTER;
+    fEnableDdr           = ENABLE_DDR;
+    
+    fEnabledSlave = false;
+    
+    if (fName) {
+        // TODO: read information from file
+    }
+    
+    InitParamMap();
 }
 
 
-bool TChipConfig::SetParamValue (const char *Name, const char *Value) 
+//___________________________________________________________________
+void TChipConfig::InitParamMap ()
 {
-  if (fSettings.find (Name) != fSettings.end()) {
-    sscanf (Value, "%d", fSettings.find(Name)->second);
-    return true;
-  }
-
-  return false;
+    fSettings["CHIPID"]           = &fChipId;
+    fSettings["RECEIVER"]         = &fReceiver;
+    fSettings["CONTROLINTERFACE"] = &fControlInterface;
+    fSettings["ENABLED"]          = &fEnabled;
+    fSettings["ITHR"]             = &fITHR;
+    fSettings["IDB"]              = &fIDB;
+    fSettings["VCASN"]            = &fVCASN;
+    fSettings["VCASN2"]           = &fVCASN2;
+    fSettings["VCLIP"]            = &fVCLIP;
+    fSettings["VRESETD"]          = &fVRESETD;
+    fSettings["IBIAS"]            = &fIBIAS;
+    fSettings["VCASP"]            =  &fVCASP;
+    fSettings["VPULSEL"]          = &fVPULSEL;
+    fSettings["VPULSEH"]          = &fVPULSEH;
+    fSettings["VRESETP"]          = &fVRESETP;
+    fSettings["VTEMP"]            = &fVTEMP;
+    fSettings["IAUX2"]            = &fIAUX2;
+    fSettings["IRESET"]           = &fIRESET;
+    fSettings["STROBEDURATION"]   = &fStrobeDuration;
+    fSettings["PULSEDURATION"]    = &fPulseDuration;
+    fSettings["STROBEDELAYCHIP"]  = &fStrobeDelay;
+    fSettings["READOUTMODE"]      = (int*)&fReadoutMode;
+    fSettings["LINKSPEED"]        = &fSerialLinkSpeed;
 }
 
 
-bool TChipConfig::SetParamValue (const char *Name, int Value) 
+//___________________________________________________________________
+bool TChipConfig::SetParamValue (const char *Name, const char *Value)
 {
-  if (fSettings.find (Name) != fSettings.end()) {
-    *(fSettings.find(Name)->second) = Value;
-    return true;
-  }
-
-  return false;
+    if (fSettings.find (Name) != fSettings.end()) {
+        sscanf (Value, "%d", fSettings.find(Name)->second);
+        return true;
+    }
+    
+    return false;
 }
 
-int TChipConfig::GetParamValue (const char *Name) 
+
+//___________________________________________________________________
+bool TChipConfig::SetParamValue (const char *Name, const int Value)
 {
-  if (fSettings.find (Name) != fSettings.end()) {
-    return *(fSettings.find(Name)->second);
-  }
-  return -1;
+    if (fSettings.find (Name) != fSettings.end()) {
+        *(fSettings.find(Name)->second) = Value;
+        return true;
+    }
+    
+    return false;
 }
 
+//___________________________________________________________________
+int TChipConfig::GetParamValue (const char *Name)
+{
+    if (fSettings.find (Name) != fSettings.end()) {
+        return *(fSettings.find(Name)->second);
+    }
+    return -1;
+}
 
-bool TChipConfig::HasEnabledSlave() {
-  if (!IsOBMaster()) return false;
-  for (int i = fChipId + 1; i <= fChipId + 6; i++) {
-    if (fConfig->GetChipConfigById(i)->IsEnabled()) return true;
-  }
-  return false;
+//___________________________________________________________________
+void TChipConfig::SetEnableSlave( const bool value )
+{
+    if ( !IsOBMaster() ) {
+        fEnabledSlave = false;
+    } else {
+        fEnabledSlave = value;
+    }
+    return;
 }
--- TChipConfig.h
+++ TChipConfig.h
@@ -1,145 +1,142 @@
 #ifndef CHIPCONFIG_H
 #define CHIPCONFIG_H
 
-#include "TConfig.h"
 #include <map>
 #include <string>
 
-class TConfig;
-
-namespace ChipConfig {     // to avoid clashes with other configs (e.g. for STROBE_DELAY)
-  const int  VCASN   = 50;
-  const int  VCASN2  = 64;
-  const int  VCLIP   = 0;
-  const int  VRESETD = 147;
-  const int  ITHR    = 51;
-  const int  IBIAS   = 64;
-  const int  VCASP   = 86;
-
-  const bool READOUT_MODE           = false;// triggered
-  const bool ENABLE_CLUSTERING      = true;
-  const int  MATRIX_READOUT_SPEED   = 1;
-  const int  SERIAL_LINK_SPEED      = 1200;
-  const bool ENABLE_SKEWING_GLOBAL  = false;
-  const bool ENABLE_SKEWING_STARTRO = false;
-  const bool ENABLE_CLOCK_GATING    = false;
-  const bool ENABLE_CMU_READOUT     = false;
-
-  // timing values, to be refined
-  const int  STROBE_DURATION = 80;       // 2 us
-  const int  STROBE_GAP      = 4000;     // .1 ms
-  const int  STROBE_DELAY    = 20;       // 500 ns
-  const int  TRIGGER_DELAY   = 0;
-  const int  PULSE_DURATION  = 500;      // 12.5 us
-
-  const int  DCLK_RECEIVER   = 10;
-  const int  DCLK_DRIVER     = 10;
-  const int  MCLK_RECEIVER   = 10;
-  const int  DCTRL_RECEIVER  = 10;
-  const int  DCTRL_DRIVER    = 10;
-
-  const int  PREVIOUS_ID        = 0x10;
-  const bool INITIAL_TOKEN      = true;
-  const bool DISABLE_MANCHESTER = false;
-  const bool ENABLE_DDR         = true;
-};
-
-
 class TChipConfig {
- private: 
-  std::map <std::string, int*> fSettings;
-  TConfig *fConfig;
-  int      fChipId;
-  int      fEnabled;                 // variable to exclude (non-working) chip from tests, default true
-  int      fReceiver;
-  int      fControlInterface;
-  // DACs used
-  int  fITHR;
-  int  fIDB;
-  int  fVCASN;
-  int  fVCASN2; 
-  int  fVCLIP;
-  int  fVRESETD;
-  int  fVCASP;
-  int  fVPULSEL;
-  int  fVPULSEH;
-  int  fIBIAS;
-  // DACs unused
-  int  fVRESETP;
-  int  fVTEMP;
-  int  fIAUX2;
-  int  fIRESET;
-  // Control register settings
-  bool fReadoutMode;        // false = triggered, true = continuous (influences busy handling)
-  bool fEnableClustering;
-  int  fMatrixReadoutSpeed;
-  int  fSerialLinkSpeed;
-  bool fEnableSkewingGlobal;
-  bool fEnableSkewingStartRO;
-  bool fEnableClockGating;
-  bool fEnableCMUReadout;
-  // Fromu settings
-  int  fStrobeDuration;
-  int  fStrobeGap;          // gap between subsequent strobes in sequencer mode
-  int  fStrobeDelay;        // delay from pulse to strobe if generated internally
-  int  fTriggerDelay;       // delay between external trigger command and internally generated strobe
-  int  fPulseDuration;
-  // Buffer current settings
-  int  fDclkReceiver;
-  int  fDclkDriver;
-  int  fMclkReceiver;
-  int  fDctrlReceiver;
-  int  fDctrlDriver;
-  // CMU / DMU settings
-  int  fPreviousId;
-  bool fInitialToken;
-  bool fDisableManchester;
-  bool fEnableDdr;
-  
- protected:
- public:
-  TChipConfig               (TConfig *config, int chipId, const char *fName = 0);
-  void InitParamMap         (); 
-  bool SetParamValue        (const char *Name, const char *Value);
-  bool SetParamValue        (const char *Name, int Value);
-  int  GetParamValue        (const char *Name) ;
-  bool IsParameter          (const char *Name) {return (fSettings.count(Name) > 0);};
-  int  GetChipId            () {return fChipId;};
-  bool IsEnabled            () {return (fEnabled != 0);};
-  void SetEnable            (bool Enabled) {fEnabled = Enabled?1:0;};
-  int  GetModuleId          () {return (fChipId & 0x70) >> 4;};
-  bool IsOBMaster           () {return ((fChipId % 8 == 0) && (GetModuleId() > 0));};
-  bool HasEnabledSlave      (); 
+    
+private:
+    std::map <std::string, int*> fSettings;
+    int      fChipId;
+    int      fEnabled;                 // variable to exclude (non-working) chip from tests, default true
+    int      fReceiver;
+    int      fControlInterface;
+    // DACs used
+    int  fITHR;
+    int  fIDB;
+    int  fVCASN;
+    int  fVCASN2;
+    int  fVCLIP;
+    int  fVRESETD;
+    int  fVCASP;
+    int  fVPULSEL;
+    int  fVPULSEH;
+    int  fIBIAS;
+    // DACs unused
+    int  fVRESETP;
+    int  fVTEMP;
+    int  fIAUX2;
+    int  fIRESET;
+    // Control register settings
+    bool fReadoutMode;        // false = triggered, true = continuous (influences busy handling)
+    bool fEnableClustering;
+    int  fMatrixReadoutSpeed;
+    int  fSerialLinkSpeed;
+    bool fEnableSkewingGlobal;
+    bool fEnableSkewingStartRO;
+    bool fEnableClockGating;
+    bool fEnableCMUReadout;
+    // Fromu settings
+    int  fStrobeDuration;
+    int  fStrobeGap;          // gap between subsequent strobes in sequencer mode
+    int  fStrobeDelay;        // delay from pulse to strobe if generated internally
+    int  fTriggerDelay;       // delay between external trigger command and internally generated strobe
+    int  fPulseDuration;
+    // Buffer current settings
+    int  fDclkReceiver;
+    int  fDclkDriver;
+    int  fMclkReceiver;
+    int  fDctrlReceiver;
+    int  fDctrlDriver;
+    // CMU / DMU settings
+    int  fPreviousId;
+    bool fInitialToken;
+    bool fDisableManchester;
+    bool fEnableDdr;
+    bool fEnabledSlave;
+    
+public:
+    TChipConfig               (int chipId, const char *fName = 0);
+    virtual ~TChipConfig(){}
+    void InitParamMap         ();
+    bool SetParamValue        (const char *Name, const char *Value);
+    bool SetParamValue        (const char *Name, const int Value);
+    int  GetParamValue        (const char *Name) ;
+    bool IsParameter          (const char *Name) {return (fSettings.count(Name) > 0);};
+    int  GetChipId            () {return fChipId;};
+    bool IsEnabled            () {return (fEnabled != 0);};
+    void SetEnable            (bool Enabled) {fEnabled = Enabled?1:0;};
+    int  GetModuleId          () {return (fChipId & 0x70) >> 4;};
+    bool IsOBMaster           () {return ((fChipId % 8 == 0) && (GetModuleId() > 0));};
+    bool HasEnabledSlave      () const { return fEnabledSlave; }
+    
+    bool GetReadoutMode          () {return fReadoutMode;};
+    bool GetEnableClustering     () {return fEnableClustering;};
+    int  GetMatrixReadoutSpeed   () {return fMatrixReadoutSpeed;};
+    int  GetSerialLinkSpeed      () {return fSerialLinkSpeed;};
+    bool GetEnableSkewingGlobal  () {return fEnableSkewingGlobal;};
+    bool GetEnableSkewingStartRO () {return fEnableSkewingStartRO;};
+    bool GetEnableClockGating    () {return fEnableClockGating;};
+    bool GetEnableCMUReadout     () {return fEnableCMUReadout;};
+    
+    int  GetTriggerDelay         () {return fTriggerDelay;};
+    int  GetStrobeDuration       () {return fStrobeDuration;};
+    int  GetStrobeDelay          () {return fStrobeDelay;};
+    int  GetPulseDuration        () {return fPulseDuration;};
+    
+    int  GetDclkReceiver         () {return fDclkReceiver;};
+    int  GetDclkDriver           () {return fDclkDriver;};
+    int  GetMclkReceiver         () {return fMclkReceiver;};
+    int  GetDctrlReceiver        () {return fDctrlReceiver;};
+    int  GetDctrlDriver          () {return fDctrlDriver;};
+    
+    int  GetPreviousId           () {return fPreviousId;};
+    bool GetInitialToken         () {return fInitialToken;};
+    bool GetDisableManchester    () {return fDisableManchester;};
+    bool GetEnableDdr            () {return fEnableDdr;};
+    
+    void SetPreviousId           (int APreviousId)         {fPreviousId   = APreviousId;};
+    void SetInitialToken         (bool AInitialToken)      {fInitialToken = AInitialToken;};
+    void SetEnableDdr            (bool AEnableDdr)         {fEnableDdr    = AEnableDdr;};
+    void SetDisableManchester    (bool ADisableManchester) {fDisableManchester = ADisableManchester;};
+    void SetEnableSlave( const bool value );
 
-  bool GetReadoutMode          () {return fReadoutMode;};
-  bool GetEnableClustering     () {return fEnableClustering;};
-  int  GetMatrixReadoutSpeed   () {return fMatrixReadoutSpeed;};
-  int  GetSerialLinkSpeed      () {return fSerialLinkSpeed;};
-  bool GetEnableSkewingGlobal  () {return fEnableSkewingGlobal;};
-  bool GetEnableSkewingStartRO () {return fEnableSkewingStartRO;};
-  bool GetEnableClockGating    () {return fEnableClockGating;};
-  bool GetEnableCMUReadout     () {return fEnableCMUReadout;};
-  
-  int  GetTriggerDelay         () {return fTriggerDelay;};
-  int  GetStrobeDuration       () {return fStrobeDuration;};
-  int  GetStrobeDelay          () {return fStrobeDelay;};
-  int  GetPulseDuration        () {return fPulseDuration;};
-
-  int  GetDclkReceiver         () {return fDclkReceiver;};
-  int  GetDclkDriver           () {return fDclkDriver;};
-  int  GetMclkReceiver         () {return fMclkReceiver;};
-  int  GetDctrlReceiver        () {return fDctrlReceiver;};
-  int  GetDctrlDriver          () {return fDctrlDriver;};
-
-  int  GetPreviousId           () {return fPreviousId;};
-  bool GetInitialToken         () {return fInitialToken;};
-  bool GetDisableManchester    () {return fDisableManchester;};
-  bool GetEnableDdr            () {return fEnableDdr;};
-
-  void SetPreviousId           (int APreviousId)         {fPreviousId   = APreviousId;};
-  void SetInitialToken         (bool AInitialToken)      {fInitialToken = AInitialToken;};
-  void SetEnableDdr            (bool AEnableDdr)         {fEnableDdr    = AEnableDdr;};
-  void SetDisableManchester    (bool ADisableManchester) {fDisableManchester = ADisableManchester;};
+private:
+    static const int  VCASN;
+    static const int  VCASN2;
+    static const int  VCLIP;
+    static const int  VRESETD;
+    static const int  ITHR;
+    static const int  IBIAS;
+    static const int  VCASP;
+    
+    static const bool READOUT_MODE;
+    static const bool ENABLE_CLUSTERING;
+    static const int  MATRIX_READOUT_SPEED;
+    static const int  SERIAL_LINK_SPEED;
+    static const bool ENABLE_SKEWING_GLOBAL;
+    static const bool ENABLE_SKEWING_STARTRO;
+    static const bool ENABLE_CLOCK_GATING;
+    static const bool ENABLE_CMU_READOUT;
+    
+    static const int  STROBE_DURATION;
+    static const int  STROBE_GAP;
+    static const int  STROBE_DELAY;
+    static const int  TRIGGER_DELAY;
+    static const int  PULSE_DURATION;
+    
+    static const int  DCLK_RECEIVER;
+    static const int  DCLK_DRIVER;
+    static const int  MCLK_RECEIVER;
+    static const int  DCTRL_RECEIVER;
+    static const int  DCTRL_DRIVER;
+    
+    static const int  PREVIOUS_ID;
+    static const bool INITIAL_TOKEN;
+    static const bool DISABLE_MANCHESTER;
+    static const bool ENABLE_DDR;
+    
 };
 
 
