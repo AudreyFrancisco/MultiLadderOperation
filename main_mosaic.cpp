4f432398692d7a3adc09c7ae27ea59774d6445a4
Andry Rakotozafindrabe <andry.malala.rakotozafindrabe@cern.ch>
2017-05-09 18:21:30 +0200

    use TSetup, TDevice and TDeviceChipOperator in the script

--- main_mosaic.cpp
+++ main_mosaic.cpp
@@ -5,72 +5,69 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <cstdint>
+#include <memory>
 #include "TReadoutBoard.h"
 #include "TReadoutBoardMOSAIC.h"
-#include "TBoardConfig.h"
-#include "TBoardConfigMOSAIC.h"
 #include "TAlpide.h"
-#include <exception>
 #include "TSetup.h"
+#include "TDevice.h"
+#include "AlpideDictionary.h"
+#include "TDeviceChipOperator.h"
+
+using namespace std;
+
+// Example of usage :
+// ./test_mosaic -v 1 -c ConfigSingleChipMOSAIC.cfg
+//
 
 int main(int argc, char** argv) {
+    
+    TSetup mySetup;
+    mySetup.DecodeCommandParameters(argc, argv);
+    mySetup.ReadConfigFile();
+    
+    shared_ptr<TDevice> theDevice = (mySetup.GetDevice()).lock();
 
-    decodeCommandParameters(argc, argv);
-	TBoardConfigMOSAIC *theBoardConfiguration;
-	TReadoutBoard      *theBoard;
-
-	// First create an instance of the Configuration
-	//theBoardConfiguration = new TBoardConfigMOSAIC("Config.cfg", 0); // The file must exists... but could be useful a constructor without param
-	// Then create an instance of the board
-
-
-	std::vector <int>      chipIDs;
-	std::vector <TAlpide*> fChips;
- 
-        for (int i = 0; i < 30; i++) chipIDs.push_back(i);
-
-        //TConfig *config = new TConfig (5);
-        TConfig *config = new TConfig (1, chipIDs);
-
-	theBoard = (TReadoutBoard *) new TReadoutBoardMOSAIC(config, (TBoardConfigMOSAIC*)config->GetBoardConfig(0));
-
-        for (int i = 0; i < config->GetNChips(); i++) {
-          fChips.push_back(new TAlpide(config->GetChipConfigById(chipIDs.at(i))));
-          fChips.at(i) -> SetReadoutBoard(theBoard);
-          theBoard     -> AddChip        (chipIDs.at(i), 0, 0);
-	}
-
-        uint16_t Value;
-
-        for (unsigned int i = 0; i < fChips.size(); i++) {
-	  // std::cout << "About to write chip " << chipIDs.at(i) << std::endl;
-          //for (int ii = 5; ii >0; ii --) {
-	  //  std::cout << "  in " << ii << " s." << std::endl;
-          //  sleep(1);
-	  //}
-          fChips.at(i)->WriteRegister (0x60d, 10);
-          try {
-            fChips.at(i)->ReadRegister (0x60d, Value);
-    	    std::cout << "Chip ID " << chipIDs.at(i) << ", Value = 0x" << std::hex << (int) Value << std::dec << std::endl;
-	  }
-            catch (std::exception &e) {
-    	    std::cout << "Chip ID " << chipIDs.at(i) << ", not answering, exception: " << e.what() << std::endl;
-	  }
-	}
-
-
+    const int nBoards = theDevice->GetNBoards( false );
+    if ( !nBoards ) {
+        cout << "No board found, exit!" << endl;
         return 0;
+    }
+    if ( nBoards != 1 ) {
+        cout << "More than one board found, exit!" << endl;
+        return 0;
+    }
 
+    const int nWorkingChips = theDevice->GetNWorkingChips();
+    if ( !nWorkingChips ) {
+        cout << "No working chip found, exit!" << endl;
+        return 0;
+    }
+    
+    shared_ptr<TReadoutBoard> theBoard = theDevice->GetBoard( 0 );
+    
+    // global reset chips
+    theBoard->SendOpCode( (std::uint16_t)AlpideOpCode::GRST );
 
-	/* Create the Chip Structures */
-	/* Reset Chips */
-	/* Initialize Chips */
-
-	/* Data Tacking */
+    // pixel matrix reset
+    theBoard->SendOpCode( (std::uint16_t)AlpideOpCode::PRST );
+    
+    // configure chip(s)
+    TDeviceChipOperator theDeviceChipOperator( theDevice );
+    theDeviceChipOperator.SetVerboseLevel( mySetup.GetVerboseLevel() );
+    theDeviceChipOperator.DoBaseConfig();
+    theDeviceChipOperator.DoConfigureFromu();
+    theDeviceChipOperator.DoConfigureCMU();
+    
+    // readout reset
+    theBoard->SendOpCode( (std::uint16_t)AlpideOpCode::RORST );
+    
+	//--- Data Tacking
 	int numberOfReadByte; // the bytes of row event
 	unsigned char *theBuffer; // the buffer containing the event
 
-	int enablePulse, enableTrigger, triggerDelay, pulseDelay, nTriggers; // variables that define the trigger/pulse
+	int enablePulse = -1, enableTrigger = -1, triggerDelay = -1, pulseDelay = -1, nTriggers = -1; // variables that define the trigger/pulse
 
 	theBuffer = (unsigned char*) malloc(200 * 1024); // allocates 200 kilobytes ...
 
@@ -82,7 +79,7 @@ int main(int argc, char** argv) {
 	theBoard->SetTriggerConfig (enablePulse, enableTrigger, triggerDelay, pulseDelay);
 	theBoard->SetTriggerSource (TTriggerSource::kTRIG_INT);
 
-	((TReadoutBoardMOSAIC *)theBoard)->StartRun(); // Activate the data taking ...
+	(dynamic_pointer_cast<TReadoutBoardMOSAIC>(theBoard))->StartRun(); // Activate the data taking ...
 
 	theBoard->Trigger(nTriggers); // Preset end start the trigger
 
@@ -97,8 +94,6 @@ int main(int argc, char** argv) {
 		}
 	}
 
-	((TReadoutBoardMOSAIC *)theBoard)->StopRun(); // Stop run
-
-	exit(0);
-        return 0;
+	(dynamic_pointer_cast<TReadoutBoardMOSAIC>(theBoard))->StopRun(); // Stop run
+    return 1;
 }
