28caddc28660d4ba784cdf719d37b0e8b177bffa
Andry Rakotozafindrabe <andry.malala.rakotozafindrabe@cern.ch>
2017-08-04 16:07:04 +0200

    add config variables that can be filled from config file

--- TChipConfig.cpp
+++ TChipConfig.cpp
@@ -6,29 +6,42 @@
 
 using namespace std;
 
+const int  TChipConfig::ITHR    = 51;
+const int  TChipConfig::IDB     = 29;
 const int  TChipConfig::VCASN   = 50;
-const int  TChipConfig::VCASN2  = 64;
+const int  TChipConfig::VCASN2  = 62;
 const int  TChipConfig::VCLIP   = 0;
 const int  TChipConfig::VRESETD = 147;
-const int  TChipConfig::ITHR    = 51;
-const int  TChipConfig::IBIAS   = 64;
 const int  TChipConfig::VCASP   = 86;
+const int  TChipConfig::VPULSEL = 0;
+const int  TChipConfig::VPULSEH = 170;
+const int  TChipConfig::IBIAS   = 64;
+
+const int  TChipConfig::VRESETP = 117;
+const int  TChipConfig::IRESET  = 50;
 
 const bool TChipConfig::READOUT_MODE           = false;
 const bool TChipConfig::ENABLE_CLUSTERING      = true;
-const int  TChipConfig::MATRIX_READOUT_SPEED   = 1;
+const bool TChipConfig::MATRIX_READOUT_SPEED   = true;
 const int  TChipConfig::SERIAL_LINK_SPEED      = (int)AlpideIBSerialLinkSpeed::IB1200;
 const bool TChipConfig::ENABLE_SKEWING_GLOBAL  = false;
 const bool TChipConfig::ENABLE_SKEWING_STARTRO = false;
 const bool TChipConfig::ENABLE_CLOCK_GATING    = false;
 const bool TChipConfig::ENABLE_CMU_READOUT     = false;
 
+const int  TChipConfig::PIXEL_MEB_MASK = 0;
+const bool TChipConfig::ENABLE_INTERNAL_STROBE = false;
+const bool TChipConfig::ENABLE_BUSY_MONITORING = true;
+const int  TChipConfig::TEST_PULSE_MODE = (int)AlpideTestPulseMode::ANALOGUE;
+const bool TChipConfig::ENABLE_TEST_STROBE = false;
+const bool TChipConfig::ENABLE_ROTATE_PULSE_LINES = false;
+
 // timing values, to be refined
 // in units of clock cycles (assuming a clock period of 25 ns)
+const int  TChipConfig::TRIGGER_DELAY   = 0;
 const int  TChipConfig::STROBE_DURATION = 80;       // 2 us
 const int  TChipConfig::STROBE_GAP      = 4000;     // .1 ms
 const int  TChipConfig::STROBE_DELAY    = 20;       // 500 ns
-const int  TChipConfig::TRIGGER_DELAY   = 0;
 const int  TChipConfig::PULSE_DURATION  = 500;      // 12.5 us
 
 const int  TChipConfig::DCLK_RECEIVER   = 10;
@@ -37,8 +50,8 @@ const int  TChipConfig::MCLK_RECEIVER   = 10;
 const int  TChipConfig::DCTRL_RECEIVER  = 10;
 const int  TChipConfig::DCTRL_DRIVER    = 10;
 
-const int  TChipConfig::PREVIOUS_ID        = 0x10;
-const bool TChipConfig::INITIAL_TOKEN      = true;
+const int  TChipConfig::PREVIOUS_ID        = 0x10; // outer barrel scenario
+const bool TChipConfig::INITIAL_TOKEN      = true; // outer barrel scenario
 const bool TChipConfig::DISABLE_MANCHESTER = false;
 const bool TChipConfig::ENABLE_DDR         = true;
 
@@ -47,19 +60,28 @@ const bool TChipConfig::ENABLE_DDR         = true;
 //___________________________________________________________________
 TChipConfig::TChipConfig()
 {
-    fChipId           = -1;
+    fChipId           = TChipConfigData::kInitValue;
     fEnabled          = true;
-    fReceiver         = -1;
-    fControlInterface = -1;
+    fEnabledSlave     = false;
+    fReceiver         = TChipConfigData::kInitValue;
+    fControlInterface = TChipConfigData::kInitValue;
     
     // fill default values from constants
+
+    fITHR    = ITHR;
+    fIDB     = IDB;
     fVCASN   = VCASN;
     fVCASN2  = VCASN2;
     fVCLIP   = VCLIP;
     fVRESETD = VRESETD;
-    fITHR    = ITHR;
+    fVCASP   = VCASP;
+    fVPULSEL = VPULSEL;
+    fVPULSEH = VPULSEH;
     fIBIAS   = IBIAS;
     
+    fVRESETP = VRESETP;
+    fIRESET  = IRESET;
+
     fReadoutMode         = READOUT_MODE;
     fEnableClustering    = ENABLE_CLUSTERING;
     fMatrixReadoutSpeed  = MATRIX_READOUT_SPEED;
@@ -68,10 +90,17 @@ TChipConfig::TChipConfig()
     fEnableClockGating   = ENABLE_CLOCK_GATING;
     fEnableCMUReadout    = ENABLE_CMU_READOUT;
     
+    fMEBmask                = PIXEL_MEB_MASK;
+    fEnableInternalStrobe   = ENABLE_INTERNAL_STROBE;
+    fEnableBusyMonitoring   = ENABLE_BUSY_MONITORING;
+    fTestPulseMode          = TEST_PULSE_MODE;
+    fEnableTestStrobe       = ENABLE_TEST_STROBE;
+    fEnableRotatePulseLines = ENABLE_ROTATE_PULSE_LINES;
+    
+    fTriggerDelay        = TRIGGER_DELAY;
     fStrobeDuration      = STROBE_DURATION;
     fStrobeGap           = STROBE_GAP;
     fStrobeDelay         = STROBE_DELAY;
-    fTriggerDelay        = TRIGGER_DELAY;
     fPulseDuration       = PULSE_DURATION;
     
     fDclkReceiver        = DCLK_RECEIVER;
@@ -84,8 +113,6 @@ TChipConfig::TChipConfig()
     fInitialToken        = INITIAL_TOKEN;
     fDisableManchester   = DISABLE_MANCHESTER;
     fEnableDdr           = ENABLE_DDR;
-    
-    fEnabledSlave = false;
     
     InitParamMap();
 }
@@ -95,17 +122,26 @@ TChipConfig::TChipConfig( const int chipId )
 {
     fChipId           = chipId;
     fEnabled          = true;
-    fReceiver         = -1;
-    fControlInterface = -1;
+    fEnabledSlave     = false;
+    fReceiver         = TChipConfigData::kInitValue;
+    fControlInterface = TChipConfigData::kInitValue;
     
     // fill default values from constants
+    
+    fITHR    = ITHR;
+    fIDB     = IDB;
     fVCASN   = VCASN;
     fVCASN2  = VCASN2;
     fVCLIP   = VCLIP;
     fVRESETD = VRESETD;
-    fITHR    = ITHR;
+    fVCASP   = VCASP;
+    fVPULSEL = VPULSEL;
+    fVPULSEH = VPULSEH;
     fIBIAS   = IBIAS;
     
+    fVRESETP = VRESETP;
+    fIRESET  = IRESET;
+   
     fReadoutMode         = READOUT_MODE;
     fEnableClustering    = ENABLE_CLUSTERING;
     fMatrixReadoutSpeed  = MATRIX_READOUT_SPEED;
@@ -114,10 +150,17 @@ TChipConfig::TChipConfig( const int chipId )
     fEnableClockGating   = ENABLE_CLOCK_GATING;
     fEnableCMUReadout    = ENABLE_CMU_READOUT;
     
+    fMEBmask                = PIXEL_MEB_MASK;
+    fEnableInternalStrobe   = ENABLE_INTERNAL_STROBE;
+    fEnableBusyMonitoring   = ENABLE_BUSY_MONITORING;
+    fTestPulseMode          = TEST_PULSE_MODE;
+    fEnableTestStrobe       = ENABLE_TEST_STROBE;
+    fEnableRotatePulseLines = ENABLE_ROTATE_PULSE_LINES;
+
+    fTriggerDelay        = TRIGGER_DELAY;
     fStrobeDuration      = STROBE_DURATION;
     fStrobeGap           = STROBE_GAP;
     fStrobeDelay         = STROBE_DELAY;
-    fTriggerDelay        = TRIGGER_DELAY;
     fPulseDuration       = PULSE_DURATION;
     
     fDclkReceiver        = DCLK_RECEIVER;
@@ -130,8 +173,6 @@ TChipConfig::TChipConfig( const int chipId )
     fInitialToken        = INITIAL_TOKEN;
     fDisableManchester   = DISABLE_MANCHESTER;
     fEnableDdr           = ENABLE_DDR;
-    
-    fEnabledSlave = false;
     
     InitParamMap();
 }
@@ -141,17 +182,26 @@ TChipConfig::TChipConfig( const int chipId, const int ci, const int rc )
 {
     fChipId           = chipId;
     fEnabled          = true;
+    fEnabledSlave     = false;
     fReceiver         = rc;
     fControlInterface = ci;
     
     // fill default values from constants
+
+    fITHR    = ITHR;
+    fIDB     = IDB;
     fVCASN   = VCASN;
     fVCASN2  = VCASN2;
     fVCLIP   = VCLIP;
     fVRESETD = VRESETD;
-    fITHR    = ITHR;
+    fVCASP   = VCASP;
+    fVPULSEL = VPULSEL;
+    fVPULSEH = VPULSEH;
     fIBIAS   = IBIAS;
     
+    fVRESETP = VRESETP;
+    fIRESET  = IRESET;
+
     fReadoutMode         = READOUT_MODE;
     fEnableClustering    = ENABLE_CLUSTERING;
     fMatrixReadoutSpeed  = MATRIX_READOUT_SPEED;
@@ -160,10 +210,17 @@ TChipConfig::TChipConfig( const int chipId, const int ci, const int rc )
     fEnableClockGating   = ENABLE_CLOCK_GATING;
     fEnableCMUReadout    = ENABLE_CMU_READOUT;
     
+    fMEBmask                = PIXEL_MEB_MASK;
+    fEnableInternalStrobe   = ENABLE_INTERNAL_STROBE;
+    fEnableBusyMonitoring   = ENABLE_BUSY_MONITORING;
+    fTestPulseMode          = TEST_PULSE_MODE;
+    fEnableTestStrobe       = ENABLE_TEST_STROBE;
+    fEnableRotatePulseLines = ENABLE_ROTATE_PULSE_LINES;
+
+    fTriggerDelay        = TRIGGER_DELAY;
     fStrobeDuration      = STROBE_DURATION;
     fStrobeGap           = STROBE_GAP;
     fStrobeDelay         = STROBE_DELAY;
-    fTriggerDelay        = TRIGGER_DELAY;
     fPulseDuration       = PULSE_DURATION;
     
     fDclkReceiver        = DCLK_RECEIVER;
@@ -176,8 +233,6 @@ TChipConfig::TChipConfig( const int chipId, const int ci, const int rc )
     fInitialToken        = INITIAL_TOKEN;
     fDisableManchester   = DISABLE_MANCHESTER;
     fEnableDdr           = ENABLE_DDR;
-    
-    fEnabledSlave = false;
     
     InitParamMap();
 }
@@ -191,6 +246,7 @@ void TChipConfig::InitParamMap()
     fSettings["ENABLED"]                = &fEnabled;
     fSettings["RECEIVER"]               = &fReceiver;
     fSettings["CONTROLINTERFACE"]       = &fControlInterface;
+
     fSettings["ITHR"]                   = &fITHR;
     fSettings["IDB"]                    = &fIDB;
     fSettings["VCASN"]                  = &fVCASN;
@@ -201,23 +257,40 @@ void TChipConfig::InitParamMap()
     fSettings["VPULSEL"]                = &fVPULSEL;
     fSettings["VPULSEH"]                = &fVPULSEH;
     fSettings["IBIAS"]                  = &fIBIAS;
+
     fSettings["VRESETP"]                = &fVRESETP;
-    fSettings["VTEMP"]                  = &fVTEMP;
-    fSettings["IAUX2"]                  = &fIAUX2;
+    //fSettings["VTEMP"]                  = &fVTEMP; // TODO: uncomment when default value is known (see also TAlpide::BaseConfigDACs() )
+    //fSettings["IAUX2"]                  = &fIAUX2; // TODO: uncomment when default value is known (see also TAlpide::BaseConfigDACs() )
     fSettings["IRESET"]                 = &fIRESET;
+
     fSettings["READOUTMODE"]            = (int*)&fReadoutMode;
     fSettings["ENABLECLUSTERING"]       = (int*)&fEnableClustering;
-    fSettings["MATRIXREADOUTSPEED"]     = &fMatrixReadoutSpeed;
+    fSettings["MATRIXREADOUTSPEED"]     = (int*)&fMatrixReadoutSpeed;
     fSettings["LINKSPEED"]              = &fSerialLinkSpeed;
     fSettings["ENABLESKEWINGGLOBAL"]    = (int*)&fEnableSkewingGlobal;
     fSettings["ENABLESKEWINGSTARTRO"]   = (int*)&fEnableSkewingStartRO;
     fSettings["ENABLECLOCKGATING"]      = (int*)&fEnableClockGating;
     fSettings["ENABLECMUREADOUT"]       = (int*)&fEnableCMUReadout;
+
+    fSettings["PIXELMEBMASK"]           = &fMEBmask;
+    fSettings["ENABLEINTERNALSTROBE"]   = (int*)&fEnableInternalStrobe;
+    fSettings["ENABLEBUSYMONITORING"]   = (int*)&fEnableBusyMonitoring;
+    fSettings["TESTPULSEMODE"]          = &fTestPulseMode;
+    fSettings["ENABLETESTSTROBE"]       = (int*)&fEnableTestStrobe;
+    fSettings["ENABLEROTATEPULSELINES"] = (int*)&fEnableRotatePulseLines;
+
+    fSettings["TRIGGERDELAY"]           = &fTriggerDelay;
     fSettings["STROBEDURATION"]         = &fStrobeDuration;
     fSettings["STROBEGAP"]              = &fStrobeGap;
     fSettings["STROBEDELAYCHIP"]        = &fStrobeDelay;
-    fSettings["TRIGGERDELAY"]           = &fTriggerDelay;
     fSettings["PULSEDURATION"]          = &fPulseDuration;
+
+    fSettings["DCLKRECEIVER"]          = &fDclkReceiver;
+    fSettings["DCLKDRIVER"]          = &fDclkDriver;
+    fSettings["MCLKRECEIVER"]          = &fMclkReceiver;
+    fSettings["DCTRLRECEIVER"]          = &fDctrlReceiver;
+    fSettings["DCTRLDRIVER"]          = &fDctrlDriver;
+    
     fSettings["DISABLEMANCHESTER"]      = (int*)&fDisableManchester;
     fSettings["ENABLEDDR"]              = (int*)&fEnableDdr;
 }
--- TChipConfig.h
+++ TChipConfig.h
@@ -14,6 +14,7 @@ private:
     std::map <std::string, int*> fSettings;
     int fChipId;
     int fEnabled; // variable to exclude (non-working) chip from tests, default true
+    bool fEnabledSlave;
     int fReceiver;
     int fControlInterface;
     #pragma mark - DACs used
@@ -29,23 +30,42 @@ private:
     int fIBIAS;
     #pragma mark - DACs unused
     int fVRESETP;
-    int fVTEMP;
-    int fIAUX2;
+    //int fVTEMP; // TODO: uncomment when default value is known (see also TAlpide::BaseConfigDACs() )
+    //int fIAUX2; // TODO: uncomment when default value is known (see also TAlpide::BaseConfigDACs() )
     int fIRESET;
     #pragma mark - Mode Control Register settings
     bool fReadoutMode; // false = triggered, true = continuous (influences busy handling)
     bool fEnableClustering;
-    int fMatrixReadoutSpeed;
+    bool fMatrixReadoutSpeed;
     int fSerialLinkSpeed;
     bool fEnableSkewingGlobal;
     bool fEnableSkewingStartRO;
     bool fEnableClockGating;
     bool fEnableCMUReadout;
     #pragma mark - Fromu settings
+    /// default b000 enables all three MEB slices (bits 2:0 FROMU config register 1)
+    int  fMEBmask;
+    /// strobe sequencer for continuous mode (bit 3 FROMU config register 1)
+    bool fEnableInternalStrobe;
+    /// controls if FROMU monitors the BUSY input and reject triggers when the BUSY is asserted (bit 4 FROMU config register 1)
+    bool fEnableBusyMonitoring;
+    /// type of pulsing (analog or digital) of the pixels (bit 5 FROMU config trgister 1)
+    int fTestPulseMode;
+    /// enable the automatic generation of an internal TRIGGER pulse after a PULSE command (bit 6 FROMU config register 1)
+    bool fEnableTestStrobe;
+    /// enable automatic shift and rotate selected pulse line after each PULSE signal (bit 7 FROMU config register 1)
+    bool fEnableRotatePulseLines;
+    //---- all in unit of clock cycles (assuming a clock period of 25 ns)
+    /// delay between external trigger command and internally generated strobe (bits 10:9 of the value in FROMU config register 1)
+    int fTriggerDelay;
+    //-- min. 1 clock cycle, max. 65536
+    /// (value in FROMU config register 2)
     int fStrobeDuration;
-    int fStrobeGap;    // gap between subsequent strobes in sequencer mode
-    int fStrobeDelay;  // delay from pulse to strobe if generated internally
-    int fTriggerDelay; // delay between external trigger command and internally generated strobe
+    /// gap between subsequent strobes when internal sequencer mode is activated (value in FROMU config register 3)
+    int fStrobeGap;
+    /// delay from pulse to strobe if strobe generated internally (value in FROMU pulsing register 1)
+    int fStrobeDelay;
+    /// (value in FROMU pulsing register 2)
     int fPulseDuration;
     #pragma mark - Buffer current settings
     int  fDclkReceiver;
@@ -58,7 +78,6 @@ private:
     bool fInitialToken;
     bool fDisableManchester;
     bool fEnableDdr;
-    bool fEnabledSlave;
 
 private:
     #pragma mark - private methods
@@ -84,11 +103,15 @@ public:
     void SetEnableDdr(const bool AEnableDdr )        { fEnableDdr    = AEnableDdr; }
     void SetDisableManchester( const bool ADisableManchester ) { fDisableManchester = ADisableManchester; }
     void SetEnableSlave( const bool value );
+    void SetTestPulseMode( const int value ) { fTestPulseMode = value; }
+    void SetEnableTestStrobe( const bool value ) { fEnableTestStrobe = value; }
 
     #pragma mark - checkers
     bool IsParameter(const char *Name) { return (fSettings.count(Name) > 0); }
     bool IsEnabled() const { return (fEnabled != 0); }
     bool IsOBMaster() const { return ((fChipId % 8 == 0) && (GetModuleId() > 0)); }
+    bool IsIBMode() const { return ((GetModuleId() == 0) && ((fChipId & 0xf) < 9)); }
+    bool IsOBSlave() const { return ((!IsIBMode()) && (!IsOBMaster())); }
     bool HasEnabledSlave() const { return fEnabledSlave; }
 
     #pragma mark - getters
@@ -106,6 +129,13 @@ public:
     bool GetEnableSkewingStartRO() const    { return fEnableSkewingStartRO; }
     bool GetEnableClockGating() const       { return fEnableClockGating; }
     bool GetEnableCMUReadout() const        { return fEnableCMUReadout; }
+
+    int  GetPixelMEBMask() const            { return fMEBmask; }
+    bool GetEnableInternalStrobe() const    { return fEnableInternalStrobe; }
+    bool GetEnableBusyMonitoring() const    { return fEnableBusyMonitoring; }
+    int  GetTestPulseMode() const           { return fTestPulseMode; }
+    bool GetEnableTestStrobe() const        { return fEnableTestStrobe; }
+    bool GetEnableRotatePulseLines() const  { return fEnableRotatePulseLines; }
     
     int  GetTriggerDelay() const            { return fTriggerDelay; }
     int  GetStrobeDuration() const          { return fStrobeDuration; }
@@ -123,30 +153,42 @@ public:
     bool GetDisableManchester() const       {return fDisableManchester; }
     bool GetEnableDdr() const               {return fEnableDdr; }
     
-
 private:
     #pragma mark - default value for the config
+    static const int  ITHR;
+    static const int  IDB;
     static const int  VCASN;
     static const int  VCASN2;
     static const int  VCLIP;
     static const int  VRESETD;
-    static const int  ITHR;
-    static const int  IBIAS;
     static const int  VCASP;
+    static const int  VPULSEL;
+    static const int  VPULSEH;
+    static const int  IBIAS;
     
+    static const int  VRESETP;
+    static const int  IRESET;
+
     static const bool READOUT_MODE;
     static const bool ENABLE_CLUSTERING;
-    static const int  MATRIX_READOUT_SPEED;
+    static const bool MATRIX_READOUT_SPEED;
     static const int  SERIAL_LINK_SPEED;
     static const bool ENABLE_SKEWING_GLOBAL;
     static const bool ENABLE_SKEWING_STARTRO;
     static const bool ENABLE_CLOCK_GATING;
     static const bool ENABLE_CMU_READOUT;
-    
+
+    static const int  PIXEL_MEB_MASK;
+    static const bool ENABLE_INTERNAL_STROBE;
+    static const bool ENABLE_BUSY_MONITORING;
+    static const int  TEST_PULSE_MODE;
+    static const bool ENABLE_TEST_STROBE;
+    static const bool ENABLE_ROTATE_PULSE_LINES;
+
+    static const int  TRIGGER_DELAY;
     static const int  STROBE_DURATION;
     static const int  STROBE_GAP;
     static const int  STROBE_DELAY;
-    static const int  TRIGGER_DELAY;
     static const int  PULSE_DURATION;
     
     static const int  DCLK_RECEIVER;
